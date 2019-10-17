1333c9cd99e75755a6fd82c93978b70a0b3016d0
av.novikov3 <av.novikov3@tensor.ru>
2019-10-17 14:52:12 +0300

    rename

--- personalsite_django/personalsite_django/wsgi.py
+++ personalsite_django/personalsite_django/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for PyDevDjangoProject project.
+WSGI config for personalsite_django project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PyDevDjangoProject.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "personalsite_django.settings")
 
 application = get_wsgi_application()
--- personalsite_django/testpage/urls.py
+++ personalsite_django/testpage/urls.py
@@ -1,4 +1,4 @@
-"""PyDevDjangoProject URL Configuration
+"""personalsite_django URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
